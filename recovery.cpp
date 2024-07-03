dc0fc2796b07ac3af5dcd7339db7f143d49dfbdc
Pranaya Deomani <pranayadmn@proton.me>
2024-07-04 00:08:02 +0530

    recovery: Use `ro.statix.base.version` to fetch StatiXOS version
    Change-Id: If5f7a4cb36af0ccb46dc012e3929f58a2d9381a6

--- recovery.cpp
+++ recovery.cpp
@@ -829,8 +829,7 @@ Device::BuiltinAction start_recovery(Device* device, const std::vector<std::stri
 
   // Extract the YYYYMMDD date from the full version string. Assume
   // the first instance of "-[0-9]{8}-" (if any) has the desired date.
-  std::string fullver = android::base::GetProperty("ro.statix.version", "");
-  std::string ver = fullver.substr(0,4);
+  std::string ver = android::base::GetProperty("ro.statix.base.version", "");
 
   std::vector<std::string> title_lines = {
     "StatiXOS " + ver,
